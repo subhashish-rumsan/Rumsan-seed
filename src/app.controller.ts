96a03b26e659e5cb5c9ec7cfeb21d287ca0c7815
subhashishshah <subhashish@ismt.edu.np>
2023-08-15 12:31:30 +0545

    testing swc for faster development

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('subhashish')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('greet')
+  hello(): string {
     return this.appService.getHello();
   }
 }
