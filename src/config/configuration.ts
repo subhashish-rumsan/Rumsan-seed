db7d748a564c0c4878d5bb920978886d09eedc43
subhashishshah <subhashish@ismt.edu.np>
2023-08-16 16:05:07 +0545

    added URI strategy for versioning

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,5 +2,4 @@ export default () => ({
   STATUS: process.env.NODE_ENV,
   DEV_PORT: process.env.DEV_PORT,
   PROD_PORT: process.env.PROD_PORT,
-  API_VERSION: process.env.API_VERSION,
 });
--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,18 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
   const configService = app.get(ConfigService);
-  const API_VERSION = configService.get<string>('API_VERSION') || 'v1';
   // NOTE: Setting up global prefix. By deafult the version contains api/v1
-  app.setGlobalPrefix(`/api/${API_VERSION}`);
+  app.setGlobalPrefix('api').enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   const PORT = configService.get<number>('DEV_PORT') || 3000;
   await app.listen(PORT);
 }
