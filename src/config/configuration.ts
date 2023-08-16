3985e2b44fb4ef668cd928146b9c1e01e29f6512
subhashishshah <subhashish@ismt.edu.np>
2023-08-16 13:17:47 +0545

    added version control for API

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,4 +2,5 @@ export default () => ({
   STATUS: process.env.NODE_ENV,
   DEV_PORT: process.env.DEV_PORT,
   PROD_PORT: process.env.PROD_PORT,
+  API_VERSION: process.env.API_VERSION,
 });
--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,10 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   const configService = app.get(ConfigService);
+  const API_VERSION = configService.get<string>('API_VERSION') || 'v1';
+  // NOTE: Setting up global prefix. By deafult the version contains api/v1
+  app.setGlobalPrefix(`/api/${API_VERSION}`);
   const PORT = configService.get<number>('DEV_PORT') || 3000;
-
   await app.listen(PORT);
 }
 bootstrap();
