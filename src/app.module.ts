10b46339f081be6f9d4abf83b9f8b53e485d492a
subhashishshah <subhashish@ismt.edu.np>
2023-08-16 12:04:30 +0545

    Added new moduls and file/folder restructuring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 import configuration from './config/configuration';
 import databaseConfig from './config/database.config';
 // import * as Joi from 'joi';
@@ -34,8 +33,7 @@ import databaseConfig from './config/database.config';
       //   abortEarly: true,
       // },
     }),
+    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
