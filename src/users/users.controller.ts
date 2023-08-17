0b3590a4ce37464682a4f4cb8ddbf5e5cded06ac
subhashishshah <subhashish@ismt.edu.np>
2023-08-17 11:17:48 +0545

    testing husky setup 2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,7 @@ import { Get } from '@nestjs/common';
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
   @Get()
   @Version('2')
   async findAll(): Promise<Users[]> {
