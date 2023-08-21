7fccd61e9fda1e91e4ab671907b613b0873c0a2c
subhashishshah <subhashish@ismt.edu.np>
2023-08-21 12:13:57 +0545

    Implemented prisma api response

--- src/blogs/blogs.controller.ts
+++ src/blogs/blogs.controller.ts
@@ -10,37 +10,46 @@ import {
 import { BlogsService } from './blogs.service';
 import { CreateBlogDto } from './dto/create-blog.dto';
 import { UpdateBlogDto } from './dto/update-blog.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { BlogsEntity } from './entities/blog.entity';
 
 @Controller('blogs')
+@ApiTags('Blogs')
 export class BlogsController {
   constructor(private readonly blogsService: BlogsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: BlogsEntity })
   create(@Body() createBlogDto: CreateBlogDto) {
     return this.blogsService.create(createBlogDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: BlogsEntity, isArray: true })
   findAll() {
     return this.blogsService.findAll();
   }
 
   @Get('drafts')
+  @ApiOkResponse({ type: BlogsEntity, isArray: true })
   findDrafts() {
     return this.blogsService.findDrafts();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: BlogsEntity, isArray: true })
   findOne(@Param('id') id: string) {
     return this.blogsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: BlogsEntity })
   update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDto) {
     return this.blogsService.update(+id, updateBlogDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: BlogsEntity })
   remove(@Param('id') id: string) {
     return this.blogsService.remove(+id);
   }
--- src/blogs/entities/blog.entity.ts
+++ src/blogs/entities/blog.entity.ts
@@ -1,1 +1,27 @@
-export class Blog {}
+// src/articles/entities/article.entity.ts
+
+import { Blog } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class BlogsEntity implements Blog {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty()
+  published: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
