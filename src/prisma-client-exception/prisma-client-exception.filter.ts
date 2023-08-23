519a922fb072d254d873ac504d2bc0f1ace98928
subhashishshah <subhashish@ismt.edu.np>
2023-08-23 06:36:15 +0545

    added logger in prisma filter

--- src/prisma-client-exception/prisma-client-exception.filter.ts
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -1,4 +1,4 @@
-import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { ArgumentsHost, Catch, HttpStatus, Logger } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { Prisma } from '@prisma/client';
 import { FastifyReply } from 'fastify';
@@ -6,11 +6,11 @@ import { FastifyReply } from 'fastify';
 @Catch(Prisma.PrismaClientKnownRequestError)
 export class PrismaClientExceptionFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
-    console.error(exception.message);
+    const logger = new Logger('Prisma logger');
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<FastifyReply>();
     const message = exception.message.replace(/\n/g, '');
-
+    logger.error('Error occured in prisma exception filter');
     switch (exception.code) {
       case 'P2002': {
         const status = HttpStatus.CONFLICT;
